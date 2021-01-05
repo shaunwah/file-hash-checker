9c9edcb22b33280609aee4cc063daa8637d9a3cc
Shaun Wah <39732975+shaunwah@users.noreply.github.com>
2021-01-06 00:36:05 +0800

    Initial commit

---
+++ script.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+
+$fileBlacklist = [
+
+];
+
+inputUrl:
+echo "Enter the file's URL:\r\n";
+$fileUrl = readline();
+
+if (!empty($fileUrl)) {
+    $fileName = basename($fileUrl);
+
+    try {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get($fileUrl);
+        $fileHash = $response->getHeader('ETag')[0];
+
+        echo $fileHash . "\r\n";
+    } catch (Exception $e) {
+        echo "An error occured.\r\n";
+    }
+}
+goto inputUrl;
+return;
